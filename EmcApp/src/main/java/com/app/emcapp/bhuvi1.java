1f5d3deece94eabceb9810742529671dea7c0fa0
Bhuvi-Bhuvaneshwari <Bhuvaneshwari2023@gmail.com>
2021-08-05 18:36:16 -0400

    Create project in Studio. Create to bhuvi1 for easy debugging.

---
+++ EmcApp/src/main/java/com/app/emcapp/bhuvi1.java
@@ -0,0 +1,11 @@
+package com.app.emcapp;
+
+import android.util.Log;
+
+public class bhuvi1 {
+    private static final String TAG="SUPER_AWESOME_APP";
+
+    public static void d(String message){
+        Log.d(TAG,message);
+    }
+}
